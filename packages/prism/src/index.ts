a0e7ed8c9c6cc688b7f6fc1a1b1263f4887d87bc
Tony Nhan <nkmnhan@gmail.com>
2026-05-03 21:45:10 +0700

    feat: add barrel export, fix exports map types order, all 21 tests pass, build verified

---
+++ packages/prism/src/index.ts
@@ -0,0 +1,6 @@
+export { PrismSurface } from './components/Surface/PrismSurface'
+export type { PrismSurfaceProps } from './components/Surface/PrismSurface'
+export { PrismBlur } from './components/Blur/PrismBlur'
+export type { PrismBlurProps } from './components/Blur/PrismBlur'
+
+export { clsxMerge } from './utils/clsxMerge'
